11e2c6f90c9f004901325545344a45be1f57bd77
matheusjean28 <matheusjean28@gmail.com>
2025-10-10 12:05:59 -0300

    send whole start project

---
+++ app/components/mainContainer.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native-web";
+
+
+
+
+export default function MainContainer (){
+    return(
+        <View>
+            <Text>
+                New Moviment
+            </Text>
+        </View>
+    );
+}
